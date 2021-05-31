f0422e37deb841de49bab038a93f69315ceb6e91
BhuvaneswariXcube <bhuvaneswari.gedela@xcubelabs.com>
2021-05-31 10:00:38 +0530

    TileScript has been added

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,21 +5,21 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
-    Vector3 direction;
+    Vector3 direction;// 
     public float playerSpeed;
-   
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            if(direction==Vector3.forward)
+            if (direction == Vector3.forward)
             {
                 direction = Vector3.left;
             }
--- Assets/TileManager.cs
+++ Assets/TileManager.cs
@@ -1,15 +1,18 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TileManager : MonoBehaviour
 {
-   
     public GameObject[] tiles;
-   
+
     public GameObject currentTile;
+
+    Stack<GameObject> forwardTilePool = new Stack<GameObject>();//created stack
+    Stack<GameObject> leftTilePool = new Stack<GameObject>();
+
     private static TileManager instance;
+
     public static TileManager Instance
     {
         get
@@ -19,15 +22,15 @@ public class TileManager : MonoBehaviour
                 instance = GameObject.FindObjectOfType<TileManager>();
             }
             return instance;
-
         }
+
     }
-   
-    
+
     // Start is called before the first frame update
     void Start()
     {
-       
+
+        // CreateTiles(5);
 
 
     }
@@ -35,11 +38,57 @@ public class TileManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     
+
     }
-  public void SpawnTile()
+    void CreateTiles(int value)//to create the pool of tiles
     {
+        for (int i = 0; i < value; i++)
+        {
+            forwardTilePool.Push(Instantiate(tiles[0]));
+            leftTilePool.Push(Instantiate(tiles[1]));
+            forwardTilePool.Peek().SetActive(false);
+            leftTilePool.Peek().SetActive(false);
+            forwardTilePool.Peek().name = "ForwardTile";
+            leftTilePool.Peek().name = "LeftTile";
+        }
+
+    }
+    public void AddForwardTilePool(GameObject tempObj)
+    {
+        forwardTilePool.Push(tempObj);
+        forwardTilePool.Peek().SetActive(false);
+    }
+    public void AddLeftTilePool(GameObject tempObj)
+    {
+        leftTilePool.Push(tempObj);
+        leftTilePool.Peek().SetActive(false);
+    }
+
+
+    public void SpawnTile()
+    {
+        if (forwardTilePool.Count == 0 || leftTilePool.Count == 0)
+        {
+            CreateTiles(10);
+        }
         int index = Random.Range(0, 2);
-        currentTile = Instantiate(tiles[index], currentTile.transform.GetChild(index).position, Quaternion.identity);
+        if (index == 0)
+        {
+            GameObject tempTile = forwardTilePool.Pop();
+            tempTile.SetActive(true);
+            tempTile.transform.position = currentTile.transform.GetChild(0).position;
+            currentTile = tempTile;
+        }
+        else if (index == 1)
+        {
+            GameObject temp = leftTilePool.Pop();
+            temp.SetActive(true);
+            temp.transform.position = currentTile.transform.GetChild(1).position;
+            currentTile = temp;
+        }
+
+
+
+
     }
 }
